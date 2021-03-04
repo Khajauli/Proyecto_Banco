5c6e8b2ea0cc78e316be14ee15c95f1d712ce695
jonif <jonif@LAPTOP-O2012K80>
2021-03-03 21:05:16 -0600

    Creacion de las clases "Cuenta" y "Prestamos"

---
+++ src/Prestamo.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jonif
+ */
+public class Prestamo {
+
+    private double montoPrestamo;
+    private double montoPagar;
+    private double interesAnual;
+    private double cuotaMensual;
+    private int tiempoPago;
+
+    public void calcularPrestamo() {
+        this.montoPagar = (this.montoPrestamo * this.interesAnual) + this.montoPrestamo;
+        this.cuotaMensual = this.montoPagar / tiempoPago;
+    }
+
+    public double getMontoPrestamo() {
+        return montoPrestamo;
+    }
+
+    public void setMontoPrestamo(double montoPrestamo) {
+        this.montoPrestamo = montoPrestamo;
+    }
+
+    public double getMontoPagar() {
+        return montoPagar;
+    }
+
+    public void setMontoPagar(double montoPagar) {
+        this.montoPagar = montoPagar;
+    }
+
+    public double getInteresAnual() {
+        return interesAnual;
+    }
+
+    public void setInteresAnual(double interesAnual) {
+        this.interesAnual = interesAnual;
+    }
+
+    public double getCuotaMensual() {
+        return cuotaMensual;
+    }
+
+    public void setCuotaMensual(double cuotaMensual) {
+        this.cuotaMensual = cuotaMensual;
+    }
+
+    public int getTiempoPago() {
+        return tiempoPago;
+    }
+
+    public void setTiempoPago(int tiempoPago) {
+        this.tiempoPago = tiempoPago;
+    }
+
+}
